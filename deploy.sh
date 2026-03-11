813bcca3f11af07dd43291f420dc127b0f16b650
GUERRIER Kevin <guerrier.k@gmail.com>
2026-03-11 13:43:36 +0100

    Ajustement docker deploy

--- deploy.sh
+++ deploy.sh
@@ -50,6 +50,6 @@ fi
 echo "=== Rebuilding stack ==="
 docker compose down
 docker compose -f docker-compose.jrcandev.yml build --no-cache
-docker compose up -d
+docker compose -f docker-compose.jrcandev.yml up -d
 
 echo "Deployment completed at $(date)"
